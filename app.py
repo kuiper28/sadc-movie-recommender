89d7ad6a564fdb9db89baac7e0866744e43f492b
robdin <robdin>
2022-01-26 12:27:33 +0200

    Code refactoring.

--- app.py
+++ app.py
@@ -15,25 +15,11 @@ from keras.layers import LSTM,Dense,Embedding,RepeatVector,TimeDistributed,Activ
 
 
 import altair as alt
-# import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
 import torch
 from neural_colaborative_filtering.model import NCRF
 
-
-# def local_css(file_name):
-#     with open(file_name) as f:
-#         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-# def remote_css(url):
-#     st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
-
-# def icon(icon_name):
-#     st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
-
-# local_css("style.css")
-# remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
 
 @st.cache
 def readData():
@@ -50,9 +36,6 @@ def readData():
 	X = ratings[['user_id', 'movie_id']].values
 	y = ratings['rating'].values
 	dim = (np.unique(np.array(X[:,0])).size, np.unique(np.array(X[:,1])).size)
-	# print(X)
-	# print(y)
-	# print(dim)
 
 	return (X, y, dim, movies)
 @st.cache
@@ -73,15 +56,7 @@ def make_recommendation_for_newuser(item_sim, item_idx, movies, k=5):
     similar_items_df = pd.concat([similar_items_df, movies], axis=1)  
     similar_items_df.columns = ['similarity','title']
     similar_items_df = similar_items_df.sort_values(by='similarity',ascending=False)
-
-    # print('Recommended movies for a new user (without rating history), currently looking at movie:', similar_items_df.iloc[0]['title'])
-    # print(similar_items_df[1:k+1])
     return(similar_items_df[1:k+1])     
-
-# item_sim = cosine_similarity(H)                     
-# make_recommendation_for_newuser(item_sim, item_idx=1, k=5)
-# make_recommendation_for_newuser(item_sim, item_idx=20, k=5)
-# make_recommendation_for_newuser(item_sim, item_idx=500, k=5)
 
 def getPredictionForSpecificUser(X,y,dim, movies):
 	ratings = view_all_ratings(st.session_state.user)
@@ -93,7 +68,6 @@ def getPredictionForSpecificUser(X,y,dim, movies):
 
 	count = 0
 	buff_X = np.copy(X)
-	# print(buff_X)
 	buff_y = np.copy(y)
 	for x in buff_X:
 		if x[0] == 6040:
@@ -102,10 +76,6 @@ def getPredictionForSpecificUser(X,y,dim, movies):
 
 	new_X = buff_X[:count]
 	new_y = buff_y[:count]	
-	# for x in X_user:
-	# 	new_X += x
-	# for y in y_user:
-	# 	new_y += y
 
 	new_X = np.append(buff_X[:count], X_user, axis = 0)
 	new_y = np.append(buff_y[:count], y_user)
@@ -125,7 +95,6 @@ def getPredictionForSpecificUser(X,y,dim, movies):
 
 	st.subheader("Movie recommendations")
 	recommended_movies = make_recommendation_for_an_existing_user(M_user, P_user, movies, 6039, k = 10)
-	# print(recommended_movies)
 	clean_db = pd.DataFrame(recommended_movies,columns=["title"])
 	st.table(clean_db)
 
@@ -138,7 +107,7 @@ def load_model_and_get_predictions(X, y, dim):
   P = H.dot(W.T).T
   P[P > 5] = 5.                   
   P[P < 1] = 1.
-  return P, H
+  return P
 
 @st.cache
 def make_recommendation_for_an_existing_user(initial_rating_matrix, predicted_rating_matrix, movies, user_idx, k=10):
@@ -146,7 +115,6 @@ def make_recommendation_for_an_existing_user(initial_rating_matrix, predicted_ra
 	user_prediction = pd.DataFrame(predicted_rating_matrix).iloc[user_idx,:]
 	preferred_movies = np.flip(np.argsort(np.array(user_ratings))[-k:])
 	recommended_movies = np.flip(np.argsort(np.array(user_prediction))[-k:])
-	# print(movies.iloc[recommended_movies])
 	return movies.iloc[recommended_movies]
 
 @st.cache 
@@ -179,7 +147,6 @@ def populate_databse():
 def get_movies_for_a_user():
 	ratings_columns = ["user_id", "movie_id", "rating", "time_stamp"]
 	ratings = pd.read_csv("ml-1m/ratings.dat", sep='::', names=ratings_columns)
-	# print(ratings.values[:10])
 
 def signup_func():
 	st.sidebar.subheader("Create An Account")
@@ -223,13 +190,9 @@ def try_login(username, password):
 def run_app_2(X, y, dim, movies, deep=False):
 	username = "username1"
 	movies_by_username = view_all_movies(username)
-	# print(movies_by_username)
 	if (deep == True):
-		# st.subheader("Movie recommendations")
 		recommended_movies = make_prediction_ncf(1, 10, movies)
-		# print("Recommended Movies ", recommended_movies)
 		clean_db = pd.DataFrame(recommended_movies,columns=["title"])
-		# st.table(clean_db)
 
 def run_app_lstm(username, X, y, dim, movies):
 	tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
@@ -262,7 +225,6 @@ def run_app_lstm(username, X, y, dim, movies):
 
 		pred_titles = []
 		for pred in user_predictions:
-			# print(get_movie_name(pred))
 			pred_titles.append(get_movie_name(pred)[0])
 
 		df = pd.DataFrame(pred_titles)
@@ -272,7 +234,6 @@ def run_app_lstm(username, X, y, dim, movies):
 
 
 def create_model(src_vocab, tar_vocab, src_timesteps, tar_timesteps, n_units):
-    # Create the model
     model = Sequential()
     model.add(Embedding(src_vocab, n_units, input_length=src_timesteps, mask_zero=True))
     model.add(LSTM(300, return_sequences=True))
@@ -283,42 +244,23 @@ def create_model(src_vocab, tar_vocab, src_timesteps, tar_timesteps, n_units):
 	
 def run_app_ncf(username, X, y, dim, movies):
 	movies_by_username = view_all_movies(username)
-	# print(movies_by_username)
 	if (len(movies_by_username) != 0):
 		st.subheader("Movie recommendations")
 		recommended_movies = make_prediction_ncf(int(username[-1]), 10, movies)
 		clean_db = pd.DataFrame(recommended_movies,columns=["title"])
 		st.table(clean_db)
-	
-	# st.bar_chart(user_df_group)
 
 		
 def run_app(username, X, y, dim, movies):
-	movies_by_username = view_all_movies(username)
-	# print(movies_by_username)
-	if (len(movies_by_username) != 0):
-		if st.checkbox("Make movie recommendations", key=3):
+	if st.checkbox("Make movie recommendations", key=3):
 
-			predicted_rating_matrix, H = load_model_and_get_predictions(X, y, dim)
-			initial_rating_matrix = create_rating_matrix(X,y,dim)
+		predicted_rating_matrix = load_model_and_get_predictions(X, y, dim)
+		initial_rating_matrix = create_rating_matrix(X,y,dim)
 
-			st.subheader("Movie recommendations")
-			recommended_movies = make_recommendation_for_an_existing_user(initial_rating_matrix, predicted_rating_matrix, movies, user_idx=int(username[-1]), k = 10)
-			clean_db = pd.DataFrame(recommended_movies,columns=["title"])
-			st.table(clean_db)
-	else:
-		if st.checkbox("Make movie recommendations for new user", key=4):
-			st.header("Select the movie you are currently watching!")
-			option = select_movie_currently_watching()
-			# print(option)
-			if (option != None):
-				predicted_rating_matrix, H = load_model_and_get_predictions(X, y, dim)
-				item_sim = cosine_similarity(H)
-				st.subheader("Movie recommendations")
-				recommended_movies = make_recommendation_for_newuser(item_sim, option, movies, 10)
-				clean_db = pd.DataFrame(recommended_movies,columns=["title"])
-				st.table(clean_db)
-			# getPredictionForSpecificUser(X,y,dim, movies)
+		st.subheader("Movie recommendations")
+		recommended_movies = make_recommendation_for_an_existing_user(initial_rating_matrix, predicted_rating_matrix, movies, user_idx=int(username[-1]), k = 10)
+		clean_db = pd.DataFrame(recommended_movies,columns=["title"])
+		st.table(clean_db)
 	
 def movieTable(username):
 	st.header("Add movie ratings")
@@ -415,7 +357,6 @@ def prepare_user(user_id):
   movies_data = pd.read_csv("ml-1m/movies.dat", sep="::", engine='python', header=None).to_numpy()[:, :3]
   items = ratings_data[:, :2].astype(np.int) - 1
   items, excluded_movies = get_user_movies_association(user_id, items, movies_data[:, 0])
-#   print(excluded_movies)
   return torch.tensor(items).to("cuda"), excluded_movies
 
 def make_prediction_ncf(user_id, k, movies):
@@ -437,32 +378,18 @@ def make_prediction_ncf(user_id, k, movies):
 
 
 def main():
-    # prepare_user(1)
 	(X, y, dim, movies) = readData()
-	# print("asdadddddddddddddddddddddddddddd")
-	# prepare_user(1)
-	# make_prediction_ncf()
 	st.title("Movie Recommender")
 	if "user" not in st.session_state:
-		# Will store the currently logged user
 		st.session_state.user = None
-
-	# run_app_2(X, y, dim, movies, deep=True)
 
 	# The database is already populated. See data.db.
 	# populate_databse()
 	menu = ["Login","SignUp"]
 	choice = st.sidebar.selectbox('Menu',menu)
- 
-	# with open("style.css") as f:st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 	with open("style.css") as f:
 		st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-# 	st.markdown(""" div.stButton > button:first-child {
-# background-color: #00cc00;color:white;font-size:20px;height:3em;width:50%;border-radius:10px 10px 10px 10px;
-# }
-# ??", unsafe_allow_html=True)
-	# st.markdown(""" div.stButton >  button:first-child {background-color: #00cc00;color:white;font-size:20px;height:3em;width:50%;border-radius:10px 10px 10px 10px;}""",  unsafe_allow_html=True)
  
 	if choice == "Login":
 		login_func()
